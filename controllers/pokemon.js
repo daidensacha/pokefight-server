5000d19adf78d83b86bebfa4f965d0addd76e385
daidensacha <daidensacha@gmail.com>
2022-09-06 21:20:11 +0200

    comment code block

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -55,26 +55,26 @@ const get_pokemon_by_id_info = async (req, res, next) => {
   }
 };
 
-function paginatedResults(model) {
-  return async (req, res, next) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 20;
+// function paginatedResults(model) {
+//   return async (req, res, next) => {
+//     const page = parseInt(req.query.page) || 1;
+//     const limit = parseInt(req.query.limit) || 20;
 
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    const results = {};
-    // console.log(pokemonData.length%limit);
+//     const startIndex = (page - 1) * limit;
+//     const endIndex = page * limit;
+//     const results = {};
+//     // console.log(pokemonData.length%limit);
 
-    if (startIndex > model.length) {
-      res.status(404).json({ message: 'No more pages' });
-    }
-    if (model.length % limit !== 0) {
-      results.totalPages = Math.ceil(model.length / limit);
-    } else {
-      results.totalPages = model.length / limit;
-    }
-  };
-}
+//     if (startIndex > model.length) {
+//       res.status(404).json({ message: 'No more pages' });
+//     }
+//     if (model.length % limit !== 0) {
+//       results.totalPages = Math.ceil(model.length / limit);
+//     } else {
+//       results.totalPages = model.length / limit;
+//     }
+//   };
+// }
 
 module.exports = {
   get_all_pokemon,
